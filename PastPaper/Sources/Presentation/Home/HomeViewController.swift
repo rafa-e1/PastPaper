5ce423ba7f4b40c183a922437b2d890d7373412e
Rafa <rafa71802937@gmail.com>
2023-12-07 16:27:01 +0900

    init: 맛집족보 앱 생성 및 초기 설정

---
+++ PastPaper/Sources/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  PastPaper
+//
+//  Created by Rafael on 12/7/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
